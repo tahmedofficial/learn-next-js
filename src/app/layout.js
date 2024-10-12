fd42a5a2a319dd9bda555ce4bf697d199c02a3e8
tahmedofficial <tanvirofcuo@gmail.com>
2024-10-12 21:36:05 +0600

    project setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,10 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <main>
+          {children}
+        </main>
       </body>
     </html>
   );
---
+++ src/app/posts/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+    return (
+        <div>
+            post page
+        </div>
+    );
+};
+
+export default page;
