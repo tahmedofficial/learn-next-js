3c23a033c689ccfd5c6c71a0c1413f425e9c2c87
tahmedofficial <tanvirofcuo@gmail.com>
2024-10-16 20:19:36 +0600

    added env variable

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -8,11 +8,11 @@ export const metadata = {
 };
 
 const getPost = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts")
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_url}/posts`)
     const data = await res.json();
-    if (data) {
-        redirect(`/posts/${data[0].id}`)
-    }
+    // if (data) {
+    // redirect(`/posts/${data[0].id}`)
+    // }
     return data
 }
 
